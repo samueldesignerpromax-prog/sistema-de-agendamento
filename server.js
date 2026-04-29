9ca4b5772485aa4e43172377ffca064271b0f86b
Samuel Tech IA <samueldesignerpromax@gmail.com>
2026-04-29 17:13:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,24 +4,11 @@ const path = require("path");
 
 app.use(express.json());
 
-// 🔥 Serve os arquivos da pasta public
+// 🔥 caminho absoluto correto
 app.use(express.static(path.join(__dirname, "public")));
 
-// 🔥 Rota principal (IMPORTANTE)
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-let agendamentos = [];
-
-app.post("/agendar", (req, res) => {
-  const { nome, data, hora } = req.body;
-  agendamentos.push({ nome, data, hora });
-  res.json({ mensagem: "Agendamento realizado!" });
-});
-
-app.get("/agendamentos", (req, res) => {
-  res.json(agendamentos);
 });
 
 app.listen(process.env.PORT || 3000, () => {
